91dead25f695ebdcb0df4c51fc934fb73ae80b9b
Denisovitch <denisglazin@gmail.com>
2021-09-27 09:46:12 +0300

    add- start section /section Buy Tickets. Project Museum stage 1 task1

---
+++ assets/js/gallery.js
@@ -0,0 +1,10 @@
+const pictureInnerContainer = document.querySelector('.sec_art_gal_picture-inner-container');
+
+const img = document.createElement('img');
+img.classList.add('gallery-img')
+img.src = `assets/img/galery/galery1.jpg`;
+img.alt = `galery1`;
+pictureInnerContainer.append(img);
+
+
+
